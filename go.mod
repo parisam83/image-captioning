6337f6875bdc3e14ddd30ad3961b3925174670cd
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-03 16:17:18 +0330

    feat: add sample openai chat completion

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/parisam83/image-captioning
+
+go 1.24.2
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-openai v1.39.0
+)
+
+require (
+	golang.org/x/lint v0.0.0-20241112194109-818c5a804067 // indirect
+	golang.org/x/tools v0.32.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf(".env file didn't load: %v", err)
+		return
+	}
+
+	key := os.Getenv("OPENAI_API_KEY")
+	fmt.Println(key)
+	client := openai.NewClient(key)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
